e2f0298a7216e1350c8f73615aae0f03fd0b2724
AphoomZhah <aphzhah@gmail.com>
2026-04-23 10:58:06 -0400

    Infor for Vite build

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: 'src/main.jsx',
-      name: 'EzonFinder',
-      formats: ['iife'],
-      fileName: () => 'ezon-finder.js',
-    },
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-      },
-    },
-  },
 });
